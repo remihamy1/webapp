808cff50d395241987492a1f3b4ae690f3fdfef2
VAN CAUWENBERGE Xavier <xavier.vancau@nordnet.fr>
2023-12-18 16:07:39 +0100

    fix add . issu

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const jsonServer = require('json-server');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const apiPort = 4000;
-const host = process.env.HOST || 'http://localhost'
+
 
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/')));
+
 
 const jsonServerInstance = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -16,11 +17,11 @@ const middlewares = jsonServer.defaults();
 jsonServerInstance.use(middlewares);
 jsonServerInstance.use(router);
 jsonServerInstance.listen(apiPort, () => {
-    console.log(`JSON Server is running on ${host}:${apiPort}`);
+    console.log(`JSON Server is running on http://localhost:${apiPort}`);
 });
-app.use('/', createProxyMiddleware({ target: `${host}:${apiPort}`, changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: `http://localhost:${apiPort}`, changeOrigin: true }));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on ${host}:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
