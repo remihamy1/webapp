8b3118cbd47482040d7711f8d1b24723e8d9a9be
VAN CAUWENBERGE Xavier <xavier.vancau@nordnet.fr>
2023-12-19 10:13:22 +0100

    fix branch

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const path = require('path');
 const jsonServer = require('json-server');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const apiPort = 4000;
-
+const host = process.env.HOST || 'http://localhost'
 
 const app = express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/')));
-
 
 const jsonServerInstance = jsonServer.create();
 const router = jsonServer.router('db.json');
@@ -17,11 +16,11 @@ const middlewares = jsonServer.defaults();
 jsonServerInstance.use(middlewares);
 jsonServerInstance.use(router);
 jsonServerInstance.listen(apiPort, () => {
-    console.log(`JSON Server is running on http://localhost:${apiPort}`);
+    console.log(`JSON Server is running on ${host}:${apiPort}`);
 });
-app.use('/', createProxyMiddleware({ target: `http://localhost:${apiPort}`, changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: `${host}:${apiPort}`, changeOrigin: true }));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${host}:${port}`);
 });
