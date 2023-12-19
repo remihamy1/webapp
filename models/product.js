75900a4b2715f7005beaf7440d38ee4fcdfbd08a
LouisFv <louis.fievet@ecoles-epsi.net>
2023-12-19 10:02:22 +0100

    addPaginationCssJsHtml

--- models/product.js
+++ models/product.js
@@ -11,6 +11,69 @@ function generateProductDiv(product, reviews) {
     return productDiv;
   }
 
+  const itemsPerPage = 2; // Set the number of products to display per page
+  let currentPage = 1; // Current page
+  
+  function loadProducts(categoryId) {
+    const productsDiv = document.getElementById("liste-produits");
+    productsDiv.innerHTML = ""; // Clear the existing products
+  
+    fetch(`${baseUrl}/products?_page=${currentPage}&_limit=${itemsPerPage}`)
+      .then((response) => {
+        const linkHeader = response.headers.get("Link");
+        const totalItems = parseInt(response.headers.get("X-Total-Count"));
+  
+        updatePaginationControls(totalItems);
+  
+        return response.json();
+      })
+      .then((products) => {
+        products.forEach((product) => {
+          fetch(`${baseUrl}/reviews?productId=${product.id}`)
+            .then((response) => response.json())
+            .then((reviews) => {
+              const productDiv = document.createElement("div");
+              productDiv.className = "product-item";
+              productDiv.innerHTML = `
+                              <h3 class="product-name">${product.name}</h3>
+                              <p class="product-description">${
+                                product.description
+                              }</p>
+                              <p class="product-price">${product.price} €</p>
+                              <div class="product-reviews">${generateReviewSummary(
+                                reviews
+                              )}</div>
+                          `;
+              productDiv.onclick = () => showProductDetail(product.id);
+              productsDiv.appendChild(productDiv);
+            });
+        });
+      })
+      .catch((error) => console.error("Error loading products:", error));
+  }
+  
+  function updatePaginationControls(totalItems) {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    const paginationDiv = document.querySelector(".pagination");
+    paginationDiv.innerHTML = ""; // Clear existing pagination controls
+  
+    for (let i = 1; i <= totalPages; i++) {
+      const pageLink = document.createElement("a");
+      pageLink.href = "#";
+      pageLink.textContent = i;
+      pageLink.onclick = () => {
+        currentPage = i;
+        loadProducts();
+      };
+  
+      if (i === currentPage) {
+        pageLink.classList.add("active");
+      }
+  
+      paginationDiv.appendChild(pageLink);
+    }
+  }
+  
   function loadProductsInitial() {
     fetch(`${baseUrl}/products`)
       .then((response) => response.json())
@@ -40,31 +103,7 @@ function generateProductDiv(product, reviews) {
       .then((response) => response.json())
       .then((category) => updateBreadcrumb(category.name));
   }
-
   
-function loadProducts(categoryId) {
-    fetch(`${baseUrl}/products`)
-      .then((response) => response.json())
-      .then((products) => {
-        if (categoryId != null) {
-          products = products.filter((product) => product.categoryId == categoryId);
-        }
-        const productsDiv = document.getElementById("liste-produits");
-        productsDiv.innerHTML = ""; // Clear old products
-        products.forEach((product) => {
-          fetch(`${baseUrl}/reviews?productId=${product.id}`)
-            .then((response) => response.json())
-            .then((reviews) => {
-              const productDiv = generateProductDiv(product, reviews);
-              productsDiv.appendChild(productDiv);
-            });
-        });
-      });
-    fetch(`${baseUrl}/categories/1`)
-      .then((response) => response.json())
-      .then((category) => updateBreadcrumb(category.name));
-  }
-
   function searchProducts() {
     const searchText = document.getElementById("search-box").value.toLowerCase();
     fetch(`${baseUrl}/products`)
