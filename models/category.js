18131590977e3e37f1ebe1bbd1b394a5fffef8ab
akiratomi <81370743+akiratomi@users.noreply.github.com>
2023-12-19 11:45:57 +0100

    Overide la PR 60 + fix la pagination

--- models/category.js
+++ models/category.js
@@ -1,15 +1,15 @@
 function loadCategories() {
-    fetch(`${baseUrl}/categories`)
-      .then((response) => response.json())
-      .then((categories) => {
-        const categoriesDiv = document.getElementById("categories");
-        categoriesDiv.innerHTML = "";
-        categories.forEach((category) => {
-          const categoryDiv = document.createElement("div");
-          categoryDiv.className = "category";
-          categoryDiv.textContent = category.name;
-          categoryDiv.onclick = () => loadProducts(category.id);
-          categoriesDiv.appendChild(categoryDiv);
-        });
+  fetch(`${baseUrl}/categories`)
+    .then((response) => response.json())
+    .then((categories) => {
+      const categoriesDiv = document.getElementById("categories");
+      categoriesDiv.innerHTML = "";
+      categories.forEach((category) => {
+        const categoryDiv = document.createElement("div");
+        categoryDiv.className = "category";
+        categoryDiv.textContent = category.name;
+        categoryDiv.onclick = () => loadProducts(category.id, 1);
+        categoriesDiv.appendChild(categoryDiv);
       });
-  }
+    });
+}
